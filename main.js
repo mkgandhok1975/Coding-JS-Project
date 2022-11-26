813b6ce796540b8b1a5f8a1f210f9261d85fe86c
mkgandhok1975 <109547657+mkgandhok1975@users.noreply.github.com>
2022-11-26 10:16:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var firstname="Tajinderpal"
+var middlename="Singh"
+var lastname="Gandhok"
+var a="20"
+var b="10"
+var add= a+b
+
+console.log(firstname.concat(middlename).concat(lastname));
+console.log(firstname.length);
+console.log(middlename.length);
+console.log(lastname.length);
+console.log(firstname.concat(middlename).concat(lastname)); 
+
+//length_number_of_characters//
+//variables in parenthesis_no double quotes//
+
+console.log(a.replace(20,"10"));
+console.log(a.replace(10,"5"));
+
+console.log(lastname.toLowerCase());
+
+console.log(firstname.toUpperCase());
+
+console.log(Math.sqrt(64));
+
+console.log(Math.max(10, 20, 30));
+
+console.log(Math.min(19, 36));
+
+console.log(17+25);
+
+console.log(Math.pow(4,10));
+
+console.log(typeof a);
+
+console.log(typeof middlename);
+console.log(add)
